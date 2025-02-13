6a85570b685cca9c57128ea4c827acfd8013b1ca
harivrdhn05 <harshagadde05@gmail.com>
2025-02-13 07:52:05 +0530

    Add files via upload

---
+++ DAY_1/main.py
@@ -0,0 +1,5 @@
+print("Hello may i know your name ??")
+first_name = input("Enter first name:")
+second_name = input("Enter Second name:")
+surname = input("Enter Surname:")
+print("Hi " + first_name + " " + second_name + " " + surname)
